0f4edc954555068a79dbc904251b29b8c134ab77
Matt Boler <meb0054@auburn.edu>
2019-09-26 08:32:50 -0500

    Added a comment for an idea in how to pass around feature data

--- include/FeatureTracker.hpp
+++ include/FeatureTracker.hpp
@@ -18,7 +18,7 @@ class FeatureTracker {
   public:
     FeatureTracker();
     bool process_image(cv::Mat image);
-    std::vector<std::vector<cv::Point2f> > get_data(); // idk how to extract data here, maybe use a pcl cloud instead?
+    std::vector<std::vector<cv::Point2f> > get_data(); // idk how to extract data here, maybe use a pcl cloud instead? Maybe a std::tuple?
   private:
     // Pixel values of tracked points
     std::vector<cv::Point2f> prev_pts_, cur_pts_;
