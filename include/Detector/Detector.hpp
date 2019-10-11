4a891d3cf1985674705bf37dcf97fe127c328723
Matt Boler <meb0054@auburn.edu>
2019-10-11 13:29:34 -0500

    not sure what I changed...

--- include/Detector/Detector.hpp
+++ include/Detector/Detector.hpp
@@ -2,8 +2,11 @@
 #define DETECTOR_HPP_INCLUDE
 
 #include <vector>
+#include <exception>
+#include <iostream>
 
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp> // For gftt
 
 #include "Utils/Feature.hpp"
 
@@ -20,12 +23,14 @@ class Detector {
     // Include functionality for forcing features to be 
     // spread across image.
     std::vector<Feature> detect_features(const cv::Mat image, const std::vector<Feature> previous_features);
+    std::vector<Feature> detect_features(const cv::Mat image, const std::vector<Feature> previous_features, const cv::Mat mask);
     
   private:
+    // This contains the parameters to pass to the feature detection method.
     DetectorParams params_;
 
-    // This increments every time a feature is detected
-    // Use it to assign unique ids to new features
+    // This increments every time a feature is detected.
+    // Use it to assign unique ids to new features.
     uint64_t feature_counter_;
 };
 
@@ -33,7 +38,20 @@ class DetectorParams {
   public:
     DetectorParams();
   private:
+
+    void config_();
+
+    // TODO: Move this to Tracker class
     size_t minimum_features_;
+
+    int max_corners_;
+    double quality_level_;
+    double min_distance_;
+    int block_size_;
+    // Note: Masks should be externally provided.
+    int gradient_size_;
+    bool use_harris_detector_;
+    double k_;
 };
 
 #endif
--- src/Detector/Detector.cpp
+++ src/Detector/Detector.cpp
@@ -2,6 +2,68 @@
 
 /*
  * This is the default (reference) implementation of a feature detector.
+ * It uses goodFeaturesToTrack to detect corners.
  */
 
-:
+Detector::Detector()
+{
+  // If no params are passed, use the default parameters
+  params_ = DetectorParams();
+  feature_counter_ = 0;
+}
+
+Detector::Detector(DetectorParams params)
+{
+  params_ = params;
+}
+
+std::vector<Feature> detect_features(const cv::Mat image, const std::vector<Feature> previous_features)
+{
+  std::vector<cv::Point2f> temp_output;
+  std::vector<Feature> output;
+  cv::goodFeaturesToTrack(image, temp_output,
+      params_.max_count_,
+      params_.quality_level_,
+      params_.min_distance_,
+      cv::Mat(),
+      params_.block_size_,
+      params_.gradient_size_,
+      params_.use_harris_detector_,
+      params_.k_);
+
+  for (cv::Point2f pt : output) {
+    // Convert cv::Point2f to Feature
+    Feature temp_feature(pt);
+    temp_feature.id = feature_counter_++;
+    output.push_back(temp_feature);
+  }
+
+  return output;
+}
+
+std::vector<Feature> detect_features(const cv::Mat image, const std::vector<Feature> previous_features, const cv::Mat mask)
+{
+
+}
+
+
+// Create a params object with defaults for given detector.
+// Parameters can then be changed after creation.
+// NOTE: Image height/width MUST be set before this can be used.
+DetectorParams::DetectorParams()
+{
+  config_();
+}
+
+// Set default params
+void DetectorParams::config_()
+{
+  max_count_ = 500;
+  quality_level_ = 0.01;
+  min_distance_ = 10;
+  block_size_ = 3;
+  gradient_size_ = 3;
+  use_harris_detector_ = false;
+  k_ = 0.04;
+}
+
