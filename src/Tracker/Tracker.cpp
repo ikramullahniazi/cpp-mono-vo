67850bf5379ba76f1e7743cc74ee85780ffc9d1f
Matt Boler <meb0054@auburn.edu>
2019-10-15 12:25:27 -0500

    Added skeleton for Tracker

---
+++ src/Tracker/Tracker.cpp
@@ -0,0 +1,6 @@
+#include "Tracker/Tracker.hpp"
+
+/*
+ * This is the default (reference) implementation of a feature tracker.
+ * It uses sparse optical flow to track features into the next frame.
+ */
